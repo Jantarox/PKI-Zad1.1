dbeaa76036d8274b17bd002ca87701524adfbbca
Jantarox <228807@edu.p.lodz.pl>
2022-03-02 16:14:24 +0100

    dodano metodę policz

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
+    static int policz(int a, int b){
+        return a+b;
+    }
     public static void main(String[] args) {
-
+        System.out.println("W pierwszym commicie: 2+3 = " + policz(2,3));
     }
 }
