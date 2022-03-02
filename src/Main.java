6ff92c7074049f4b585fd00bb8b3133df0ffa264
Jantarox <228807@edu.p.lodz.pl>
2022-03-02 16:32:26 +0100

    zmiany przez SourceTree

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     static int policz(int a, int b){
-        return a*b;
+        return (a+b)*(a-b);
     }
     public static void main(String[] args) {
         System.out.println("W drugim commicie: 2*3 = " + policz(2,3));
